39cd0e8a9c25ab2ec9f57aa44c5244f962204a0e
romm1of <romantupss@gmail.com>
2021-01-30 13:41:15 +0200

    fix(ci/cd): test fail

--- src/test/kotlin/net/letsgg/platform/LetsggCoreServiceApplicationTests.kt
+++ src/test/kotlin/net/letsgg/platform/LetsggCoreServiceApplicationTests.kt
@@ -2,9 +2,11 @@ package net.letsgg.platform
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
-class UserMailServiceApplicationTests {
+@ActiveProfiles("test")
+class LetsggCoreServiceApplicationTests {
 
 	@Test
 	fun contextLoads() {
