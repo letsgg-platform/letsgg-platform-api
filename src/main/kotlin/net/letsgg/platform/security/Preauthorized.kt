ed5ea8928d52b1985a1b33ed89df80e1b716b616
romm1of <romantupss@gmail.com>
2021-02-03 19:29:23 +0200

    fix typo in user permissions

--- src/main/kotlin/net/letsgg/platform/security/Preauthorized.kt
+++ src/main/kotlin/net/letsgg/platform/security/Preauthorized.kt
@@ -3,6 +3,6 @@ package net.letsgg.platform.security
 object Preauthorized {
     const val WITH_ROLE_ADMIN = "hasRole('ADMIN')"
     const val WITH_ROLE_USER = "hasRole('USER')"
-    const val WITH_AUTHORITY_READ = "hasAuthority('READ')"
-    const val WITH_AUTHORITY_MODIFY = "hasAuthority('MODIFY')"
+    const val WITH_AUTHORITY_READ = "hasAuthority('read')"
+    const val WITH_AUTHORITY_MODIFY = "hasAuthority('modify')"
 }
