cf0e0f8cea90b03541889184b0b964100ff20acb
romm1of <romantupss@gmail.com>
2021-02-03 09:50:52 +0200

    misc(email_service): minor edits

---
+++ src/main/kotlin/net/letsgg/platform/webapi/controller/UserEmailController.kt
@@ -0,0 +1,26 @@
+package net.letsgg.platform.webapi.controller
+
+import net.letsgg.platform.service.NewsletterSignUpServiceFacade
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import java.util.*
+
+@RestController
+@RequestMapping("api/email-service")
+@CrossOrigin(origins = ["*"], allowedHeaders = ["*"])
+class UserEmailController(
+    private val newsletterSignUpServiceFacade: NewsletterSignUpServiceFacade
+) {
+    @PostMapping("/newsletter-signup/{userEmail}")
+    fun subscribeForNewsletter(@PathVariable userEmail: String): ResponseEntity<Unit> {
+        newsletterSignUpServiceFacade.signUpForNewsletterAndSendWelcomeMail(userEmail)
+        return ResponseEntity.ok().build()
+    }
+
+    @GetMapping("/newsletter-unsubscribe-{id}")
+    fun unsubscribeFromNewsletter(@PathVariable("id") emailEntryId: UUID): String { //TODO. reconsider
+        val messageOnSuccessfulUnsubscribe = "Unsubscribed."
+        newsletterSignUpServiceFacade.unsubscribeFromNewsletterAndSendByeMail(emailEntryId)
+        return messageOnSuccessfulUnsubscribe
+    }
+}
