a5b8d1444dde7ca24a6c76ed9fa29af3bbcec878
romm1of <romantupss@gmail.com>
2021-04-22 21:28:10 +0300

    api: deprecate email_controller

--- src/main/kotlin/net/letsgg/platform/webapi/controller/UserEmailController.kt
+++ src/main/kotlin/net/letsgg/platform/webapi/controller/UserEmailController.kt
@@ -1,27 +1,32 @@
 package net.letsgg.platform.webapi.controller
 
-import net.letsgg.platform.service.NewsletterSignUpServiceFacade
+import net.letsgg.platform.service.newsletter.NewsletterSubscribeServiceFacade
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.util.*
 
+/**
+ * TODO. replace for email preferences settings.
+ * */
+
 @RestController
 @RequestMapping("api/email-service")
 @CrossOrigin(origins = ["*"], allowedHeaders = ["*"])
+@Deprecated(level = DeprecationLevel.WARNING, message = "Will be replaced by user email preferences")
 class UserEmailController(
-    private val newsletterSignUpServiceFacade: NewsletterSignUpServiceFacade
+    private val newsletterSubscribeServiceFacade: NewsletterSubscribeServiceFacade
 ) {
     @PostMapping("/newsletter-signup/{userEmail}")
     fun subscribeForNewsletter(@PathVariable userEmail: String): ResponseEntity<Unit> {
-        newsletterSignUpServiceFacade.signUpForNewsletterAndSendWelcomeMail(userEmail)
+        newsletterSubscribeServiceFacade.signUpForNewsletterAndSendWelcomeMail(userEmail)
         return ResponseEntity(HttpStatus.OK)
     }
 
     @GetMapping("/newsletter-unsubscribe-{id}")
-    fun unsubscribeFromNewsletter(@PathVariable("id") emailEntryId: UUID): String { //TODO. reconsider
+    fun unsubscribeFromNewsletter(@PathVariable("id") emailEntryId: UUID): String {
         val messageOnSuccessfulUnsubscribe = "Unsubscribed."
-        newsletterSignUpServiceFacade.unsubscribeFromNewsletterAndSendByeMail(emailEntryId)
+        newsletterSubscribeServiceFacade.unsubscribeFromNewsletterAndSendByeMail(emailEntryId)
         return messageOnSuccessfulUnsubscribe
     }
 }
