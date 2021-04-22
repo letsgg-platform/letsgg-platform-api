6b8f40686e26a3f2faab76da582df258ae474e7a
romm1of <romantupss@gmail.com>
2021-04-22 22:43:53 +0300

    test: fix bean validation test fail

--- src/test/kotlin/net/letsgg/platform/JpaBeanValidationTest.kt
+++ src/test/kotlin/net/letsgg/platform/JpaBeanValidationTest.kt
@@ -2,12 +2,13 @@ package net.letsgg.platform
 
 import net.letsgg.platform.entity.EmailEntry
 import net.letsgg.platform.repository.EmailEntryRepo
+import net.letsgg.platform.service.newsletter.NewsletterSubscribeServiceFacade
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
-import org.junit.platform.commons.util.StringUtils
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.context.annotation.Import
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit.jupiter.SpringExtension
@@ -21,6 +22,8 @@ import org.springframework.transaction.TransactionSystemException
 class JpaBeanValidationTest(
     @Autowired private val emailEntryRepo: EmailEntryRepo
 ) {
+    @MockBean
+    lateinit var newsletterSubscribeServiceFacade: NewsletterSubscribeServiceFacade
 
     @Test
     fun throwsExceptionWithInvalidBean() {
